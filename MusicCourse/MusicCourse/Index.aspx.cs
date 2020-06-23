77eae3202429c3d981444053895696b174ebd0ab
cancalkan <cancalkaniletisim@gmail.com>
2020-06-23 13:20:02 +0300

    Music Course Registration Project
    You have to restore Sql BackUp then change the connection  strings for your mssql server name.

---
+++ MusicCourse/MusicCourse/Index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MusicCourse
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.Tbl_MusicStudentsTableAdapter dt = new DataSet1TableAdapters.Tbl_MusicStudentsTableAdapter();
+            dt.AddMusicStudent(TxtName.Text, TxtPhone.Text, TxtMail.Text, DropDownList1.SelectedItem.ToString(), DropDownList2.SelectedItem.ToString());
+        }
+    }
+}
