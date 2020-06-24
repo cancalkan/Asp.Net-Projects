ed8ab511e99de0c94db564e5b69cc2b5a651164c
cancalkan <cancalkaniletisim@gmail.com>
2020-06-24 18:20:36 +0300

    AgriCulture Site By Can Calkan
    You have to do change the data connection with my database.bak for use it.

---
+++ AgriCulture/AgriCulture/Default.Aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AgriCulture
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.Tbl_InfoTableAdapter dt = new DataSet1TableAdapters.Tbl_InfoTableAdapter();
+            Repeater1.DataSource = dt.GetInfo();
+            Repeater1.DataBind();
+        }
+    }
+}
