da64bf33982e17c498ad1c1ac7bc8a3856c90420
cancalkan <cancalkaniletisim@gmail.com>
2020-07-01 03:15:43 +0300

    Asp.Net Project
    You have to do restore this backup and change the connection string for using that project

---
+++ Master Login Form/Master Login Form/Datas.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Datas.aspx.cs" Inherits="Master_Login_Form.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            There is Customer Values on Here 
+        </div>
+    </form>
+</body>
+</html>
---
+++ Master Login Form/Master Login Form/Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Master_Login_Form
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-BNF3RCA;Initial Catalog=MasterLoginForm;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblMasterLogin where username=@p1 AND password=@p2", connection);
+            command.Parameters.AddWithValue("@p1", TextBox1.Text);
+            command.Parameters.AddWithValue("@p2", TextBox2.Text);
+            SqlDataReader dr = command.ExecuteReader();
+            if (dr.Read())
+            {
+                Response.Redirect("Datas.Aspx");
+            }
+            else
+            {
+                Response.Write("<script language=javascript>alert('Wrong UserName or PassWord');</script>");
+
+            }
+            connection.Close();
+        }  
+        
+    }
+}
